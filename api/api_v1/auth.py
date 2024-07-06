0155fdb6feca77f8664093ed0eadefe531fe9a6b
saatarko <saatar@tut.by>
2024-07-06 16:01:26 +0300

    create auth router2

--- api/api_v1/auth.py
+++ api/api_v1/auth.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
 
+from api.api_v1.fastapi_user_routers import fastapi_users
+from api.dependencies.authentication.backend import auth_backend
 from core.config import settings
+
+
+# создаем роутеры для аутентификации пользователя
 
 router = APIRouter(
     prefix=settings.api.v1.auth,
     tags=["Auth"],
+)
+
+router.include_router(
+    router=fastapi_users.get_auth_router(auth_backend),
 )
--- api/api_v1/fastapi_user_routers.py
+++ api/api_v1/fastapi_user_routers.py
@@ -2,8 +2,8 @@ from fastapi_users import FastAPIUsers
 
 from core.models import User
 from core.type.user_id import UserIdType
-from .user_manager import get_user_manager
-from .backend import auth_backend
+from api.dependencies.authentication.user_manager import get_user_manager
+from api.dependencies.authentication.backend import auth_backend
 
 # заготовка для маршрутов
 
