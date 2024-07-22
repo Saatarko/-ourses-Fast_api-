bea91d4f43f2bd885888a2c3df02787d34a400d2
Saatarko <saatarko@tut.by>
2024-07-22 16:02:03 +0300

    modify groups logic,add gropue and other routes

--- main.py
+++ main.py
@@ -59,7 +59,10 @@ main_app.include_router(
 
 
 
+#
+# if __name__ == "__main__":
+#     uvicorn.run("main:main_app", host="127.0.0.1", port=8000, reload=True)
+#     # uvicorn.run(main_app)
 
-if __name__ == "__main__":
-    uvicorn.run("main:main_app", host="127.0.0.1", port=8000, reload=True)
-    # uvicorn.run(main_app)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app")
