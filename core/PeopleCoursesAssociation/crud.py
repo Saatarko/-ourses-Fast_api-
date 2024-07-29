09e157c13fe737f48cfd49350cb566fe0017cd47
Saatarko <saatarko@tut.by>
2024-07-29 13:48:04 +0300

    celery on

--- core/PeopleCoursesAssociation/crud.py
+++ core/PeopleCoursesAssociation/crud.py
@@ -88,6 +88,4 @@ async def create_peoples_and_courses(
         # Отправляем email через Celery
         send_email_task.delay(subject, course_leader_email, body)
 
-        # для запуска celery используем celery -A celery_config.app worker -l info -P eventlet
-
     return peoples_and_courses
