76ed18b0b9215f61f6ab23755d4be8902f287e94
Saatarko <saatarko@tut.by>
2024-07-29 14:14:30 +0300

    celery on

--- core/PeopleCoursesAssociation/crud.py
+++ core/PeopleCoursesAssociation/crud.py
@@ -86,6 +86,6 @@ async def create_peoples_and_courses(
         await send_email(subject, course_leader_email, body)
 
         # Отправляем email через Celery
-        # send_email_task.delay(subject, course_leader_email, body)
+        send_email_task.delay(subject, course_leader_email, body)
 
     return peoples_and_courses
--- main.py
+++ main.py
@@ -79,6 +79,6 @@ main_app.include_router(
     test_router,
 )
 #
-# if __name__ == "__main__":
-#     uvicorn.run("main:main_app", host="127.0.0.1", port=8000, reload=True)
-#     # uvicorn.run(main_app)
+if __name__ == "__main__":
+    uvicorn.run("main:main_app", host="127.0.0.1", port=8000, reload=True)
+    # uvicorn.run(main_app)
